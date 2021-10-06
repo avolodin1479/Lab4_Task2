e2b3f088fc471836af12e4fc08ba00cc23e68284
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-06 08:36:55 +0600

    Добавьте файлы проекта.

---
+++ Lab4_Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sumP = 0;
+            int sumN = 0;
+
+            int N;
+            do
+            {
+                
+                N = Convert.ToInt32(Console.ReadLine());
+                if (N > 0)
+                {
+                    sumP = sumP+1;
+                }
+                if (N < 0)
+                {
+                    sumN = sumN+1;
+                }
+                              
+
+            } while (N != 0);
+
+            if (sumP > sumN)
+            {
+                Console.WriteLine("Больше положительных чисел.");
+            }
+            if (sumP < sumN)
+            {
+                Console.WriteLine("Больше отрицательных чисел.");
+            }
+
+            if (sumP == sumN)
+            {
+                Console.WriteLine("Количество положительных и отрицательных чисел равно");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
